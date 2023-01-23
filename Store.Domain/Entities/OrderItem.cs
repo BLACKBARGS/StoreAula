c2a6e1d806db6f99326eb222b3a7ae293c140d35
Francys Rodrigues <blackgsgofsr@gmail.com>
2023-01-22 21:41:05 -0300

    Changes to be committed: 	modified:   Store.Domain/Entities/OrderItem.cs  Changes not staged for commit: 	modified:   Store.Domain/Entities/Order.cs

--- Store.Domain/Entities/OrderItem.cs
+++ Store.Domain/Entities/OrderItem.cs
@@ -1,4 +1,6 @@
 using System;
+using Flunt.Notifications;
+using Flunt.Validations;
 
 namespace Store.Domain.Entities;
 
@@ -6,11 +8,23 @@ public class OrderItem : Entity
 {
     public OrderItem(Product product, int quantity)
     {
+        Validate(product, quantity);
+
         Product = product;
         Price = Product != null ? product.Price : 0;
         Quantity = quantity;
     }
-    
+
+    private void Validate(Product product, int quantity)
+    {
+        AddNotifications(
+            new Contract<Notification>()
+                .Requires()
+                .IsNotNull(product, "Product", "Invalid Product")
+                .IsGreaterThan(quantity, 0, "Quantity", "Products cannot stay in 0 items!")
+        );
+    }
+
     public Product? Product { get; private set; }
     public decimal Price { get; private set; }
     public int Quantity { get; private set; }
